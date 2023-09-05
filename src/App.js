2184a68d1c043081654cfbf0893cf049db2e6edf
Syed Farhan <syedfarhan596@gmail.com>
2023-09-05 15:21:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { persons } from './data';
+import { useState } from 'react';
 function App() {
+  const [person,setPerson] = useState(persons)
+  const deleteItem = (id)=> {
+    let newPerson = person.filter((item)=> item.id !== id)
+    setPerson(newPerson)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container h-75 w-50 p-2'>
+      <div className=" justify-content-center border ">
+        <h1>BirthDay Reminder</h1>
+        {person.map((items)=>{
+          const {id,name,alt,age,img} = items
+          return(
+            <div key={id} className="d-flex justify-content-between btn btn-light m-2 p-2 border-bottom ">
+              <div>
+                <h4>name:{name}</h4>
+                <h6>age:{age}</h6>
+                <img alt={alt} width='70' height='70' className="rounded-circle" src={img}></img>
+              </div>
+              <button className='btn btn-light' onClick={()=>deleteItem(id)}>Remove</button>
+            </div>
+          )
+        })}
+      </div>
+      <button className='btn btn-lg btn-danger w-100' onClick={()=> setPerson([])}> Clear All</button>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,36 @@
+import img1 from './static/myPic.jpg'
+import img2 from './static/faraz.jpg'
+import img3 from './static/faseeh.jpg'
+import img4 from './static/anas.jpg'
+
+export const persons = [
+    {
+        id:1,
+        alt:'farhan',
+        name:'syed farhan',
+        age:20,
+        img:img1
+    },
+    {
+        id:2,
+        alt:'faraz',
+        name:'syed faraz',
+        age:20,
+        img:img2
+    },
+    {
+        id:3,
+        alt:'faseeh',
+        name:'syed faseeh',
+        age:20,
+        img:img3
+    },
+    {
+        id:4,
+        alt:'anas',
+        name:'syed anas',
+        age:20,
+        img:img4
+    },
+]
+
